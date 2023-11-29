12cb2c0fbbe956f2df679446d9ba8d0a742b41da
DawidMilek <dawidmilek.m@gmail.com>
2023-11-29 18:57:39 +0100

    dodano quick sort

--- algorytmy/Form1.cs
+++ algorytmy/Form1.cs
@@ -21,9 +21,9 @@ namespace algorytmy
 
         public void BubbleSort(int[] array)
         {
-            for(int i = 0; i < array.Length; i++)
+            for (int i = 0; i < array.Length; i++)
             {
-                for(int j = i + 1; j < array.Length; j++)
+                for (int j = i + 1; j < array.Length; j++)
                 {
                     if (array[i] > array[j])
                     {
@@ -39,10 +39,10 @@ namespace algorytmy
         {
             int n = array.Length;
 
-            for(int i = 0; i < n-1; i++)
+            for (int i = 0; i < n - 1; i++)
             {
                 int index_min = i;
-                for(int j = i+1; j < n; j++)
+                for (int j = i + 1; j < n; j++)
                 {
                     if (array[j] < array[index_min])
                     {
@@ -56,29 +56,104 @@ namespace algorytmy
             }
         }
 
-        public void Przez_Wstawianie(int []array)
+        public void Przez_Wstawianie(int[] array)
         {
-            for(int i =0; i < array.Length;i++)
+            for (int i = 0; i < array.Length; i++)
             {
                 int obecny = array[i];
                 int j = i - 1;
 
-                while(j >= 0 && array[j] > obecny)
+                while (j >= 0 && array[j] > obecny)
                 {
-                    array[j+1] = array[j];
+                    array[j + 1] = array[j];
                     j = j - 1;
                 }
 
-                array[j+1] = obecny;
+                array[j + 1] = obecny;
             }
         }
 
+        public void Szybkie(int[] array, int lewy, int prawy)
+        {
+            var i = lewy;
+            var j = prawy;
+            var srodek = array[(lewy+prawy)/2];
+
+            while (i <= j)
+            {
+                while (array[i] < srodek)
+                { i++; }
+                while (array[j] > srodek)
+                { j--; }
+
+                if (i <= j)
+                {
+                    int tmp = array[i];
+                    array[i] = array[j];
+                    array[j] = tmp;
+                    i++;
+                    j--;
+                }
+            }
+
+            if (lewy < j)
+            { Szybkie(array, lewy, j); }
+
+            if (i < prawy)
+            { Szybkie(array, i, prawy); }
+        }
+        /*nie działa
+        public void scalenie(int poczatek, int koniec)
+        {
+            int []tmp_array = new int[this.array.Length];
+            for(int i = poczatek; i <= koniec; i++)
+            {
+                tmp_array[i] = this.array[i]; //kopia
+            }
+
+            int p = poczatek;
+            int q = (poczatek+koniec)/2+1;
+            int r = poczatek;
+            while(p <= (poczatek+koniec)/2 && q <= koniec)
+            {
+                if (tmp_array[p] < tmp_array[q])
+                {
+                    this.array[r] = tmp_array[p];
+                    r++;
+                    p++;
+                }
+                else
+                {
+                    this.array[r] = tmp_array[q];
+                    r++;
+                    q++;
+                }
+            }
+
+            while(p <= (poczatek+koniec)/2)
+            {
+                this.array[r] = tmp_array[p];
+                r++;
+                p++;
+            }
+        }
+
+        public void Przez_Scalenie(int poczatek, int koniec)
+        {
+            if(poczatek <  koniec)
+            {
+                Przez_Scalenie(poczatek, (poczatek + koniec) / 2);
+                Przez_Scalenie((poczatek + koniec) / 2 + 1, koniec);
+                scalenie(poczatek,koniec);
+            }
+        }
+        */
         private void D_generuj_Click(object sender, EventArgs e)
         {
-            array = new int[10];
+            array = new int[15];
             Random rnd = new Random();
 
-            for(int i = 0; i < 10; i++)
+            for (int i = 0; i < array.Length; i++)
             {
                 this.array[i] = rnd.Next(100);
                 D_liczby_wygenerowane.Items.Add(this.array[i]);
@@ -92,7 +167,7 @@ namespace algorytmy
 
         private void D_bombelkowe_CheckedChanged(object sender, EventArgs e)
         {
-            
+
         }
 
         private void D_Przez_wybor_CheckedChanged(object sender, EventArgs e)
@@ -122,7 +197,7 @@ namespace algorytmy
 
         private void D_liczby_posortowane_SelectedIndexChanged(object sender, EventArgs e)
         {
-           D_liczby_posortowane.Items.Clear();
+            D_liczby_posortowane.Items.Clear();
         }
 
         private void D_wykres_Click(object sender, EventArgs e)
@@ -131,21 +206,31 @@ namespace algorytmy
         }
         private void D_sortuj_Click(object sender, EventArgs e)
         {
-            if(D_bombelkowe.Checked)
+            if (D_bombelkowe.Checked)
             {
                 BubbleSort(this.array);
             }
-            else if(D_Przez_wstawienie.Checked)
+            else if (D_Przez_wstawienie.Checked)
             {
                 Przez_Wstawianie(this.array);
             }
-            else if(D_Przez_wybor.Checked)
+            else if (D_Przez_wybor.Checked)
             {
                 Przez_Wybor(this.array);
             }
-            
+            else if (D_szybkie.Checked)
+            {
+                Szybkie(this.array, 0, this.array.Length - 1);
+            }
+            else if(D_Przez_scalenie.Checked)
+            {
+                //Przez_Scalenie(0, this.array.Length);
+            }
 
-            for(int i = 0; i < this.array.Length; i++)
+            D_liczby_posortowane.Items.Clear();
+
+
+            for (int i = 0; i < this.array.Length; i++)
             {
                 D_liczby_posortowane.Items.Add(this.array[i]);
             }
